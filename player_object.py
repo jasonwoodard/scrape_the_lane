89a9ec051d5fdb34b696ec427cf7ad16a186b459
me@jasonwoodard.com <Wxx^QWbMxgz$p4t>
2018-08-17 11:18:51 -0400

    touching up export templates

--- player_object.py
+++ player_object.py
@@ -2,12 +2,11 @@
 class Player(object):
 
     PlayerRowHeader = '''
-        Id, Name, Yr, Ht, Pos, Gms, Min, FG, FG%, 3P, 3P%, FT, FT%, OR, DR, TR, Ast, Stl, Blk, TO, PF, +/-, Pts
-        '''
+Id, Name, Yr, Ht, Pos, Gms, Min, FG, FG%, 3P, 3P%, FT, FT%, OR, DR, TR, Ast, Stl, Blk, TO, PF, +/-, Pts'''
 
     PlayerRowTemplate = '''
-        {{id}}, {{name}}, {{year}}, {{height}}, {{position}}, {{games}}, {{min}}, {{fg}}, {{fg_pct}}, {{three_p}}, {{three_p_pct}}, {{ft}}, {{ft_pct}}, {{offense_rating}}, {{defense_rating}}, {{tr}}, {{ast}}, {{stl}}, {{blk}}, {{to}}, {{pf}}, {{plus_minus}}, {{pts}}
-        '''
+{{id}}, {{name}}, {{year}}, {{height}}, {{position}}, {{games}}, {{min}}, {{fg}}, {{fg_pct}}, {{three_p}}, {{three_p_pct}}, {{ft}}, {{ft_pct}}, {{offense_rating}}, {{defense_rating}}, {{tr}}, {{ast}}, {{stl}}, {{blk}}, {{to}}, {{pf}}, {{plus_minus}}, {{pts}}
+'''
 
     def __init__(self, player_row):
         self.row = player_row
