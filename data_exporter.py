4d3cfbb0e1a0ecbfafe235298d3729f041d516c8
me@jasonwoodard.com <Wxx^QWbMxgz$p4t>
2018-08-20 00:14:57 -0400

    csv export

--- data_exporter.py
+++ data_exporter.py
@@ -17,3 +17,19 @@ class DataExporter(object):
         # Get the rows template
         row_template = header + DataExporter.RowsTemplate.format(template)
         return pystache.render(row_template, rows)
+
+    def write_to_csv(self, header, row_objects):
+        filename = 'output.csv'
+        values = self._get_rows_from_objects(row_objects)
+        rows = [header]
+        rows.extend(values)
+        with open(filename, 'wb') as f:
+            writer = csv.writer(f)
+            writer.writerows(rows)
+
+    @staticmethod
+    def _get_rows_from_objects(row_objects):
+        rows = []
+        for obj in row_objects:
+            rows.append(obj.emit_row())
+        return rows
