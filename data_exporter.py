e4b4e6e0ce83eb855a5f6a5db3cf3b741918998d
me@jasonwoodard.com <Wxx^QWbMxgz$p4t>
2018-09-13 21:12:02 -0400

    comments

--- data_exporter.py
+++ data_exporter.py
@@ -1,18 +1,15 @@
 import csv
 
-# [AMS] ADD THE FOLLOWING CODE TO NAME OUTPUT FILES BASED ON DATE.
-import glob2
 from datetime import datetime
 
 
 class DataExporter(object):
 
     def write_to_csv(self, header, row_objects):
-        # filename = 'output.csv'
         values = self._get_rows_from_objects(row_objects)
         rows = [header]
         rows.extend(values)
-        with open(datetime.now().strftime("%Y-%m-%d")+".csv", 'w') as f:
+        with open(self._get_file_name(), 'w') as f:
             writer = csv.writer(f)
             writer.writerows(rows)
 
@@ -22,3 +19,7 @@ class DataExporter(object):
         for obj in row_objects:
             rows.append(obj.emit_row())
         return rows
+
+    @staticmethod
+    def _get_file_name():
+        return '{:%Y-%m-%d}.csv'.format(datetime.now())
