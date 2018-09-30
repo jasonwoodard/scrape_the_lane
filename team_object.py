9dec00aa78d807e889c1d68c03b08644ee796aee
me@jasonwoodard.com <Wxx^QWbMxgz$p4t>
2018-09-29 21:53:44 -0700

    add team attributes

--- team_object.py
+++ team_object.py
@@ -4,4 +4,25 @@ class Team(object):
         self.name = ''
         self.id = -1
         self.conference_id = -1
-        # TODO(jwoodard) scrape additional fields
+        self.gms = -1
+        self.min = 0
+        self.fg = ''
+        self.fg_made = 0
+        self.fg_attempted = 0
+        self.fg_pct = 0
+        self.three_point = ''
+        self.three_point_made = 0
+        self.three_point_attempted = 0
+        self.three_point_pct = 0
+        self.ft = ''
+        self.ft_attempted = 0
+        self.ft_made = 0
+        self.ft_pct = 0
+        self.oreb = 0
+        self.dr = 0
+        self.tr = 0
+        self.ast = 0
+        self.blk = 0
+        self.pf = 0
+        self.plus_minus = 0
+        self.pts = 0
